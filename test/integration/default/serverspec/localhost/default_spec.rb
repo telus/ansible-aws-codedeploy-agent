e833ae8c943715181dc1bd8c6c246d4b48ce8ffd
Ben Visser <theodore.r.visser@gmail.com>
2016-02-23 13:37:49 -0800

    fixing tests

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'ansible-codedeploy-agent::default' do
+describe 'ansible-aws-codedeploy-agent::default' do
 
   describe service('codedeploy-agent') do
     it { should be_enabled }
